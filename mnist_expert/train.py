69a7a1a8be55913acdcfe12721351ed2de320c53
Thomas Edlich <thomas.edlich@tum.de>
2017-12-19 00:41:48 +0100

    removes miscommited directory

--- mnist_expert/train.py
+++ mnist_expert/train.py
@@ -5,10 +5,20 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 def weight_variable(shape):
+    """
+    creates weight variable
+    :param shape: 
+    :return: 
+    """
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
 def bias_variable(shape):
+    """
+    creates bias variable
+    :param shape: 
+    :return: 
+    """
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
